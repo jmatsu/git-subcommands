1bf65e27c6b606b33da5aaf987f7bf5153d9859a
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2015-12-20 02:16:15 +0900

    Fixed git-root

--- src/git-root-dir.sh
+++ src/git-root-dir.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-ROOT_DIR=$(git rev-parse --git-dir)
-
-if [ "${ROOT_DIR%/*}" = "${ROOT_DIR}" ]; then
-  ROOT_DIR=$(pwd)
-else
-  ROOT_DIR="${ROOT_DIR%/*}"
-fi
-
-echo $ROOT_DIR
+git rev-parse --show-toplevel
